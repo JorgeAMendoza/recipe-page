b2ef6ed633d1c6a7bd262d0f383f50cf350f508d
Jorge A Mendoza II <jorgemendozaiidev@gmail.com>
2024-03-07 18:10:09 -0600

    content
    render the content on the page by creating a `omellete.json` file that
    contains all information for the recipe which is extracted by the
    `App.tsx` component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,80 @@
-import { useState } from 'react'
-import blazeImage from './assets/blaze.jpg?w=300&h=300&format=webp&imagetools'
-import handIcon from './assets/hand.svg'
-import utilStyle from '@/styles/utils/container.module.css'
+import recipeData from '@/assets/omellete.json'
+import omletteImageDesktop from '@/assets/images/image-omelette.jpeg?w=300&format=webp&imagetools'
+import omletteImageMobile from '@/assets/images/image-omelette.jpeg?w=150&format=webp&imagetools'
 import style from './app.module.css'
-const techIcons = ['react-icon.svg', 'typescript-icon.svg', 'vite-icon.svg']
 
 function App() {
-  const [boops, setBoops] = useState(0)
-  const [playAnimation, setPlayAnimation] = useState(false)
   return (
     <main className={style.app}>
-      <section aria-label="starter introduction" className={style.introduction}>
-        <div className={utilStyle.container}>
-          <div>
-            <h1>Vite + React + TypeScript</h1>
-            <ul className={style.techList}>
-              {techIcons.map((icon) => (
-                <li key={icon}>
-                  {' '}
-                  <img src={`/icons/${icon}`} alt="" />
-                </li>
-              ))}
-            </ul>
-          </div>
+      <picture>
+        <source
+          srcSet={omletteImageDesktop}
+          type="image/webp"
+          media="(width >= 768px)"
+        />
+        <img src={omletteImageMobile} alt="Omelette" />
+      </picture>
+      <h1>Simple Omellete Recipe</h1>
+      <p>
+        An easy and quick dish, perfect for any meal. This classic omelette
+        combines beaten eggs cooked to perfection, optionally filled with your
+        choice of cheese, vegetables, or meats.
+      </p>
 
-          <p className={style.info}>
-            Welcome to my <strong>Vite + React + TypeScript</strong> template
-            setup. Please check out the <code>README.md</code> for more
-            information on the configuration!!
-          </p>
-        </div>
+      <div>
+        <p>Preparation time</p>
+        <ul>
+          <li>
+            <p>
+              <strong>Total: </strong>Approximately 10 minutes
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>Preparation: </strong>5 minutes
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>Cooking: </strong>5 minutes
+            </p>
+          </li>
+        </ul>
+      </div>
+
+      <section aria-label="ingredients for the simple omlette recipe">
+        <h2>Ingredients</h2>
+        <ul>
+          {recipeData.ingredients.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
       </section>
 
-      <section
-        aria-label="interactive dog petting"
-        className={style.petGoodBoy}
-      >
-        <div className={utilStyle.container}>
-          <h2>
-            Give the good boy <br />
-            some boops
-          </h2>
+      <section aria-label="instructions for the simple omlette recipe">
+        <h2>Instructions</h2>
+        <ol>
+          {Object.entries(recipeData.instructions).map(([key, value]) => (
+            <li key={key}>
+              <p>
+                <strong>{key}: </strong>
+                {value}
+              </p>
+            </li>
+          ))}
+        </ol>
+      </section>
 
-          <div className={style.boopCounter}>
-            <button
-              onClick={() => {
-                if (playAnimation) return
-                setBoops((b) => b + 1)
-                setPlayAnimation(true)
-              }}
-              disabled={playAnimation}
-            >
-              Boop
-            </button>
-            <p>Boops: {boops}</p>
-          </div>
-
-          <div className={style.boopers}>
-            <img
-              src={handIcon}
-              alt=""
-              className={style.hand}
-              data-boop={playAnimation}
-              onAnimationEnd={() => setPlayAnimation(false)}
-            />
-            <img src={blazeImage} alt="Very good dog" className={style.blaze} />
-          </div>
-        </div>
+      <section aria-label="nutrition of the simple omlette recipe">
+        <h2>Nutrition</h2>
+        <table>
+          {Object.entries(recipeData.nutrition).map(([key, value]) => (
+            <tr key={key}>
+              <td>{key}</td>
+              <td>{value}</td>
+            </tr>
+          ))}
+        </table>
       </section>
     </main>
   )
