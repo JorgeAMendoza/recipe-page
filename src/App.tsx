b2e50f12618106cae9d14fffce811235730d4b00
Jorge A Mendoza II <jorgemendozaiidev@gmail.com>
2024-03-09 10:05:10 -0600

    mobile style
    apply the styles for mobile view of the page, for the ingredients,
    instructions, and opening sections. Adjusted the `HTML` syntax according
    to the Figma design and best practices.

--- src/App.tsx
+++ src/App.tsx
@@ -14,70 +14,77 @@ function App() {
         />
         <img src={omletteImageMobile} alt="Omelette" />
       </picture>
-      <section>
-        <h1>Simple Omellete Recipe</h1>
-        <p>
-          An easy and quick dish, perfect for any meal. This classic omelette
-          combines beaten eggs cooked to perfection, optionally filled with your
-          choice of cheese, vegetables, or meats.
-        </p>
+      <div className={style.content}>
+        <section aria-label="omlette recipe introduction">
+          <h1>Simple Omelette Recipe</h1>
+          <p>
+            An easy and quick dish, perfect for any meal. This classic omelette
+            combines beaten eggs cooked to perfection, optionally filled with
+            your choice of cheese, vegetables, or meats.
+          </p>
 
-        <div>
-          <p>Preparation time</p>
+          <div className={style.preparationTime}>
+            <p>Preparation time</p>
+            <ul>
+              <li>
+                <p>
+                  <strong>Total</strong>: Approximately 10 minutes
+                </p>
+              </li>
+              <li>
+                <p>
+                  <strong>Preparation</strong>: 5 minutes
+                </p>
+              </li>
+              <li>
+                <p>
+                  <strong>Cooking</strong>: 5 minutes
+                </p>
+              </li>
+            </ul>
+          </div>
+        </section>
+
+        <section
+          aria-label="ingredients for the simple omlette recipe"
+          className={style.ingredients}
+        >
+          <h2>Ingredients</h2>
           <ul>
-            <li>
-              <p>
-                <strong>Total: </strong>Approximately 10 minutes
-              </p>
-            </li>
-            <li>
-              <p>
-                <strong>Preparation: </strong>5 minutes
-              </p>
-            </li>
-            <li>
-              <p>
-                <strong>Cooking: </strong>5 minutes
-              </p>
-            </li>
+            {recipeData.ingredients.map((item) => (
+              <li key={item}>{item}</li>
+            ))}
           </ul>
-        </div>
-      </section>
+        </section>
 
-      <section aria-label="ingredients for the simple omlette recipe">
-        <h2>Ingredients</h2>
-        <ul>
-          {recipeData.ingredients.map((item) => (
-            <li key={item}>{item}</li>
-          ))}
-        </ul>
-      </section>
+        <section
+          aria-label="instructions for the simple omlette recipe"
+          className={style.instructions}
+        >
+          <h2>Instructions</h2>
+          <ol>
+            {Object.entries(recipeData.instructions).map(([key, value]) => (
+              <li key={key}>
+                <p>
+                  <strong>{key}</strong>: {value}
+                </p>
+              </li>
+            ))}
+          </ol>
+        </section>
 
-      <section aria-label="instructions for the simple omlette recipe">
-        <h2>Instructions</h2>
-        <ol>
-          {Object.entries(recipeData.instructions).map(([key, value]) => (
-            <li key={key}>
-              <p>
-                <strong>{key}: </strong>
-                {value}
-              </p>
-            </li>
-          ))}
-        </ol>
-      </section>
-
-      <section aria-label="nutrition of the simple omlette recipe">
-        <h2>Nutrition</h2>
-        <table>
-          {Object.entries(recipeData.nutrition).map(([key, value]) => (
-            <tr key={key}>
-              <td>{key}</td>
-              <td>{value}</td>
-            </tr>
-          ))}
-        </table>
-      </section>
+        <section aria-label="nutrition of the simple omlette recipe">
+          <h2>Nutrition</h2>
+          <table>
+            {Object.entries(recipeData.nutrition).map(([key, value]) => (
+              <tr key={key}>
+                <td>{key}</td>
+                <td>{value}</td>
+              </tr>
+            ))}
+          </table>
+        </section>
+      </div>
     </main>
   )
 }
